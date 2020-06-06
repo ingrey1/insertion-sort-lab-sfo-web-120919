0e6fcfd8b5dae97362896233f9f8cf2ab75d9d86
github username <ingrey1@gmail.com>
2020-06-05 20:42:30 -0700

    findMinAndRemove

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function findMinAndRemove(array){
 
     let max = array[0]
+    let index = 0
+    array.forEach(el => {
+        if (el < max) {
+            max = el
+            index = i
+        }    
+    })
+
+    array.splice(index, 1)
+
+
     
 
 }
